3dbce674d40ca7f1a7e8822b9d0571e352fe2ed6
unz <ugurakbas.unz@gmail.com>
2023-08-19 11:04:53 +0300

    version 1

---
+++ src/p03_/Soru.java
@@ -0,0 +1,18 @@
+package p03_;
+
+import java.util.Scanner;
+
+public class Soru {
+    public static void main(String[] args) {
+
+        // Soru 1-Kullanicidan bir sayi isteyin,
+        //        sayiyi kontrol edip 5 ile bolunebiliyorsa
+        //        “Sayi 5’in tam kati” yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir sayi giriniz");
+        int girilenSayi = scanner.nextInt();
+
+        if (girilenSayi%5==0) System.out.println("Sayi 5’in tam kati");
+    }
+}
