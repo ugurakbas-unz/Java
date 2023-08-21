aa956e9c9748ee54d6c79dfece01a52812d637b5
unz <ugurakbas.unz@gmail.com>
2023-08-21 11:03:33 +0300

    version 1

---
+++ src/p03_ifElseStatements/Question3.java
@@ -0,0 +1,24 @@
+package p03_ifElseStatements;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+
+        //Soru 3-Kullanicidan bir sayi alin, sayi 3 ile bolunuyorsa ”Uc ile bolunebilen
+        //       sayi”, 5 ile bolunebiliyorsa “Bes ile bolunebilen sayi” yazdirin.
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir tamsayı giriniz");
+        int girilenSayi= scanner.nextInt();
+
+        if (girilenSayi%5==0){
+            System.out.println("Bes ile bolunebilen sayi");
+
+        }
+        if (girilenSayi%3==0) {
+            System.out.println("Uc ile bolunebilen sayı");
+        }
+    }
+}
