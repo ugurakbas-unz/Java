9fc17227b54ce3896a0d1b6ae9538ef6e622c039
unz <ugurakbas.unz@gmail.com>
2023-08-22 07:28:09 +0300

    version 1

---
+++ src/p03_ifElseStatements/Questions4.java
@@ -0,0 +1,21 @@
+package p03_ifElseStatements;
+
+import java.util.Scanner;
+
+public class Questions4 {
+
+    public static void main(String[] args) {
+
+        // Soru 4-Kullanicidan bir ucgenin 3 kenar uzunlugunu alin, ucgen eskenar ise
+        //       “Eskenar ucgen” yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter tusunu kullanarak ucgen için ayrı ayrı uc kenar uzunlugu giriniz");
+        int kenar1= scanner.nextInt();
+        int kenar2= scanner.nextInt();
+        int kenar3= scanner.nextInt();
+
+        if (kenar1==kenar2 && kenar1==kenar3 && kenar1>0)
+            System.out.println("Eskenar ucgen");
+    }
+}
