0552505355c2bf53ec717fd6f23672de5c6b218a
unz <ugurakbas.unz@gmail.com>
2023-08-29 14:03:32 +0300

    version1

---
+++ src/p03_ifElseStatements/Question10.java
@@ -0,0 +1,35 @@
+package p03_ifElseStatements;
+
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args) {
+
+        //Soru 1-Kullanicidan cinsiyetini ve yasini alin,
+        //       Kadin, 60 yas ve uzeri , Erkek 65 yas ve uzeri emekli olabilir.
+        //       Cinsiyet ve yasini dikkate alarak
+        //       “Emekli olabilirsin” veya
+        //       “Emekli olmak icin .. Yil daha calisman gerekir” yazdirin.(metod)
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("emeklilik hesabı için E yada K olarak cinsiyetinizi giriniz");
+        char cinsiyet=scanner.next().toUpperCase().charAt(0);
+        System.out.println("emeklilik hesabı için yasınızı giriniz");
+        int yas= scanner.nextInt();
+
+        if (cinsiyet=='E'){
+            if (yas>=65){
+                System.out.println("Emekli olabilirsin");
+            }else System.out.println("emekli olabilmen için " + (65-yas ) +" yıl daha calısman gerekiyor");
+
+        } else if (cinsiyet=='K') {
+            if (yas>=60){
+                System.out.println("Emekli olabilirsin");
+            }else System.out.println("emekli olabilmen için " + (60-yas ) +" yıl daha calısman gerekiyor");
+
+
+        }else
+            System.out.println("cinsiyeti E yada K olarak giriniz");
+
+    }
+}
