f34a28fb29b519db5ca8490702e1f3f1503dfeb3
unz <ugurakbas.unz@gmail.com>
2023-09-01 18:14:05 +0300

    version1

---
+++ src/p03_ifElseStatements/Question13.java
@@ -0,0 +1,31 @@
+package p03_ifElseStatements;
+
+import java.util.Scanner;
+
+public class Question13 {
+    public static void main(String[] args) {
+
+        // Soru 4-Kullanicidan mesafeyi kilometre olarak alin ve istedigi birimi sorun,
+        //        istedigi birim metre veya santimetre ise cevirip yazdirin, yoksa
+        //        “istediginiz birim sisteme kayitli degil” yazdirin.(method)
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("kilometre olarak mesafa giriniz");
+        double km= scanner.nextDouble();
+        System.out.println("cevirmek istediginiz birim için" +
+                "\n metre yada santimetre yazınız" );
+        String birim= scanner.next();
+
+
+        if (birim.equalsIgnoreCase("metre")){
+            System.out.println("sonuç: "+km+" kilometre "+ (km*1000)+" metre eder");
+
+        }else if(birim.equalsIgnoreCase("santimetre")){
+            System.out.println("sonuç:"+km+" kilometre "+ (km*100000)+" santimetre eder");
+
+        }else
+            System.out.println("istediginiz birim sisteme kayitli degil");
+
+    }
+}
